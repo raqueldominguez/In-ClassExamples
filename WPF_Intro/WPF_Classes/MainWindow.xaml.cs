99f9d501ca8106612ba365b0ac27ec62771a7190
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2021-09-13 11:45:03 -0500

    Created class for student and file

---
+++ WPF_Intro/WPF_Classes/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Classes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string id, fname, lname, gpa, probation, balance;
+
+            id = txtID.Text;
+            fname = txtFirstName.Text;
+            lname = txtLastName.Text;
+            //probation = txtProbation.Text;
+            gpa = txtGPA.Text;
+            balance = txtBursar.Text;
+
+            Student stud = new Student(Convert.ToInt32(id), fname, lname, Convert.ToDouble(balance));
+            stud.GPA = Convert.ToDouble(gpa);
+            /*            if (probation.ToLower() == "yes")
+                        {
+                            stud.IsOnProbation = true;
+                        }
+                        else
+                        {
+                            stud.IsOnProbation = false;
+                        }*/
+
+            stud.IsOnProbation = chkProbation.IsChecked.Value;
+
+            lstStudents.Items.Add(stud);
+        }
+
+        private void lstStudents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Student selected = (Student)lstStudents.SelectedItem;
+
+            MessageBox.Show(selected.CheckBalance().ToString("C"));
+        }
+    }
+}
---
+++ WPF_Intro/WPF_Classes/Student.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Classes
+{
+    public class Student
+    {
+        public int SoonerID {get;set;}
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public bool IsOnProbation {get;set;}
+        public double GPA {get;set;}
+
+        private double BursarBalance;
+
+        // public List<string> something { get; set;}
+        // public UniversityCourse course { get; set;}
+
+        /// <summary>
+        ///  Default/ empty constructor
+        /// </summary>
+        public Student()
+        {
+            SoonerID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 0;
+            //something = new List<string>();
+            //couse = new UniversityCourse();
+        }
+
+        public Student (int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            IsOnProbation = false;
+            GPA = 0;
+            this.BursarBalance = BursarBalance;
+        }
+
+        public Student(string fName, string lName)
+        {
+            SoonerID = 0;
+            FirstName = fName;
+            LastName = lName;
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 0;
+        }
+
+        /// <summary>
+        /// Make a payment towards the Bursar Balance
+        /// </summary>
+        /// <param name="amount"></param> A positive amount to deduct from the bursar balance
+        public void MakePayment(double amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("Amount must be positve");
+                //throw new Exception("Amount must be positive");
+            }
+
+            BursarBalance -= amount;
+        }
+
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName} ({SoonerID.ToString("N0")}";
+        }
+    }
+}
