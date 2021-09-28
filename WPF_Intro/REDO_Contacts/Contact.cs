3f80d2f29933ffc2600825eacf691e05c8fa2718
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2021-09-28 15:03:34 -0500

    Worked on contact wpf again

---
+++ WPF_Intro/REDO_Contacts/Contact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REDO_Contacts
+{
+    public class Contact
+    {
+        public int Id {get;set;}
+
+        public string Firstname {get;set;}
+
+        public string Lastname {get;set;}
+
+        public string Email {get;set;}
+
+        public string Photo {get;set;}
+
+        public Contact()
+        {
+            Id = 0;
+            Firstname = string.Empty;
+            Lastname = string.Empty;
+            Email = string.Empty;
+            Photo = string.Empty;
+        }
+
+        public Contact(int id, string firstname, string lastname, string email, string photo)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            Email = email;
+            Photo = photo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Lastname}, {Firstname}"; 
+        }
+
+    }
+}
---
+++ WPF_Intro/REDO_Contacts/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace REDO_Contacts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] lines = File.ReadAllLines("contacts.txt");
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] pieces = line.Split("|");
+
+                Contact contact = new Contact(Convert.ToInt32(pieces[0]), pieces[1], pieces[2], pieces[3], pieces[4]);
+                lstContacts.Items.Add(contact);
+            }
+        }
+
+        private void btnDetails_Click(object sender, RoutedEventArgs e)
+        {
+            Contact c = (Contact)lstContacts.SelectedItem;
+            txtFirstName.Text = c.Firstname;
+            txtLastName.Text = c.Lastname;
+            txtEmail.Text = c.Email;
+
+            var uri = new Uri(c.Photo);
+            var img = new BitmapImage(uri);
+            imgPhoto.Source = img;
+        }
+    }
+}
