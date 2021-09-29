d8e29a7cb74501e3f781f0e49c7c357ff6fde9d4
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2021-09-28 20:45:18 -0500

    redid contact list participation

---
+++ WPF_Intro/AGAIN_CONTACTS/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AGAIN_CONTACTS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] lines = File.ReadAllLines("contacts.txt");
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] pieces = line.Split("|");
+
+                Contact contact = new Contact(Convert.ToInt32(pieces[0]), pieces[1], pieces[2], pieces[3], pieces[4]);
+
+                lstContacts.Items.Add(contact);
+            }
+        }
+
+        private void btnDetails_Click(object sender, RoutedEventArgs e)
+        {
+            Contact selecteditem = (Contact)lstContacts.SelectedItem;
+            txtFirstName.Text = selecteditem.FirstName;
+            txtLastName.Text = selecteditem.LastName;
+            txtEmail.Text = selecteditem.Email;
+
+            var uri = new Uri(selecteditem.Photo);
+            var img = new BitmapImage(uri);
+            imgPhoto.Source = img;
+        }
+    }
+}
