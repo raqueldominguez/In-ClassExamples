77a4aed4bf1944a4bf613c242e45b2867c0d4d38
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2021-10-06 11:44:34 -0500

    Worked on pokemon api

---
+++ WPF_Intro/JSON_Pokemon/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Pokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cboPokemon.Items.Clear();
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon?offset=00&limit=1200").Result;
+
+                PokemonAPI api = JsonConvert.DeserializeObject<PokemonAPI>(json);
+
+                foreach (Pokemon item in api.results)
+                {
+                    cboPokemon.Items.Add(item);
+                }
+            }
+
+        }
+
+        private void btnToggle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cboPokemon_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Pokemon pokemon = (Pokemon)cboPokemon.SelectedItem;
+
+            txtHeight.Text = pokemon.height;
+            txtWeight.Text = pokemon.weight;
+        }
+    }
+}
---
+++ WPF_Intro/JSON_Pokemon/Pokemon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_Pokemon
+{
+    public class PokemonAPI
+    {
+        public List<Pokemon> results { get; set; }
+        
+    }
+
+    public class Pokemon
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string weight { get; set; }
+        public string height { get; set; }
+
+        public override string ToString()
+        {
+            return $"{name}"; 
+        }
+
+        public Pokemon()
+        {
+            name = string.Empty;
+            weight = string.Empty;
+            height = string.Empty;
+        }
+    }
+}
