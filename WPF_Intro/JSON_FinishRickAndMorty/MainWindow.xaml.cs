798e4e110c4898116340fbd6d1d2a5dfa736526b
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2021-10-11 11:45:30 -0500

    worked on pulling in all characters into the combobox

---
+++ WPF_Intro/JSON_FinishRickAndMorty/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_FinishRickAndMorty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cboCharacters.Items.Clear();
+
+            using (var client = new HttpClient())
+            {
+                Info i = new Info();
+                i.pages = 34;
+                for (int j = 0; j < i.pages; i++)
+                {
+                    string jsonData = client.GetStringAsync("https://rickandmortyapi.com/api/character?pages{i}").Result;
+
+                    RickAndMortyAPI api = JsonConvert.DeserializeObject<RickAndMortyAPI>(jsonData);
+
+                    foreach (Character item in api.results)
+                    {
+                        cboCharacters.Items.Add(item);
+                    }
+                }                 
+            }
+            
+        }
+
+        private void cboCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Character selected = (Character)cboCharacters.SelectedItem;
+
+            lblCharacterName.Content = selected.name;
+
+            imgCharacter.Source = new BitmapImage(new Uri(selected.image));
+
+            MessageBox.Show($"The url is {selected.url}");
+
+        }
+    }
+}
---
+++ WPF_Intro/JSON_FinishRickAndMorty/RickAndMortyAPI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_FinishRickAndMorty
+{
+    public class RickAndMortyAPI
+    {
+        public Info info { get; set; }
+
+        public List<Character> results { get; set; }
+
+    }
+
+    public class Character
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string status { get; set; }
+
+        public string species { get; set; }
+
+        public string type { get; set; }
+
+        public string gender { get; set; }
+
+        public string image { get; set; }
+
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return $"{name} ({id})";
+        }
+
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+
+        public int pages { get; set; }
+
+        public string next { get; set; }
+
+        public string prev { get; set; }
+
+        public Info()
+        {
+            count = 0;
+            pages = 0;
+            next = string.Empty;
+            prev = string.Empty;
+        }
+
+    }
+}
