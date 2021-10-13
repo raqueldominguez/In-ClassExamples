f22fe3596a8fef2c0ea9283a6fbd3cd6d44f2b50
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2021-10-13 11:33:59 -0500

    Finished GOT quote participation

---
+++ WPF_Intro/(P) GOT Quote/GOTAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P__GOT_Quote
+{
+    public class GOTAPI
+    {
+        public string quote { get; set; }
+        public string character { get; set; }
+
+        public override string ToString()
+        {
+            return $"{quote} -{character}";
+        }
+    }
+
+}
--- WPF_Intro/(P) GOT Quote/MainWindow.xaml.cs
+++ WPF_Intro/(P) GOT Quote/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,35 @@ namespace _P__GOT_Quote
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<GOTAPI> quotessss = new List<GOTAPI>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnQuote_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://got-quotes.herokuapp.com/quotes";
+            using (var client = new HttpClient())
+            {
+
+                string json = client.GetStringAsync(url).Result;
+                GOTAPI quote = JsonConvert.DeserializeObject<GOTAPI>(json);
+                lstQuotes.Items.Add(quote);
+
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (var clien = new HttpClient())
+            {
+                string FileName = "GOT_Quotes.json";
+                string jsonString = JsonConvert.SerializeObject(quotessss);
+                File.WriteAllText(FileName, jsonString);
+            }          
+
         }
     }
 }
