35200d7d44abaced46e52e7abb4b6c7705023d79
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2021-11-15 11:42:54 -0600

    worked on MVC in class example

---
+++ WPF_Intro/MVC_Example/Controllers/OUController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Example.Controllers
+{
+    public class OUController : Controller
+    {
+        // GET: OU
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Students()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student()
+                {
+                                    FirstName = "Micah",
+                LastName = "Tison",
+                SoonerID = 5;
+        }
+    }   
+        
+            
+            return View(Students);
+}   
+    }
+}
