41c78e2aef0b02852385306d76884359b763aae9
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2021-11-17 10:53:52 -0600

    Created project for MVC participation

--- WPF_Intro/MVC_Example/Models/Student.cs
+++ WPF_Intro/MVC_Example/Models/Student.cs
@@ -8,7 +8,7 @@ namespace MVC_Example.Models
     public class Student
     {
         public string FirstName { get; set; }
-            public string LastName { get; set; }
+        public string LastName { get; set; }
         public int SoonerID { get; set; }
     }
 }
